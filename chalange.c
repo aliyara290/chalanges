9d39d9a534e4f000bcf0b5179941a0005a2b5bf0
Ali Yara <ali.yara.fr@gmail.com>
2024-09-06 11:38:06 +0200

    add mini project

---
+++ chalange.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+void sum(int a, int b) {
+    a + b;
+}
+int main() {
+    sum(6,8);
+    return 0;
+}
---
+++ mini-project/task-1.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main() {
+    
+    printf("entrer 10 nombers: ");
+    int nombers[10];
+    for(int i = 0; i < 10; i++) {
+        scanf("%d", &nombers[i]);
+    }
+    int somme = 0;
+    for(int i = 0; i < 10; i++) {
+        
+        if(nombers[i] % 5 == 0 || nombers[i] % 3 == 0) {
+            
+            somme +=nombers[i];
+        }
+    }
+    
+    printf("le somme est : %d", somme);
+
+    return 0;
+}
